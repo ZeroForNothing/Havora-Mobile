32f0df8dce5828112384c223ed67de9f4e697cd7
chahal-Ismail <65466249+chahal-Ismail@users.noreply.github.com>
2020-06-29 19:34:22 +0300

    Add SignIn Text Validation

--- screens/signInActivity.js
+++ screens/signInActivity.js
@@ -1,7 +1,9 @@
 import React, {  Component } from 'react';
-import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
+import { StyleSheet, View, Text, TextInput, TouchableOpacity, ImageBackground, ActivityIndicator } from 'react-native';
 
 import {UserSignIn} from '../js/signin';
+import { Formik } from 'formik';
+import * as yup from 'yup';
 
 export default class signInActivity extends Component {
     constructor(props) {
@@ -14,32 +16,73 @@ export default class signInActivity extends Component {
 
   render() {
    
+    const validationSchema = yup.object().shape({
+      email: yup
+        .string()
+        .label('Email')
+        .email()
+        .required(),
+
+      password: yup
+        .string()
+        .label('Password')
+        .required()
+        .min(2, 'Seems a bit short...')
+        .max(10, 'We prefer insecure system, try a shorter password.'),
+    });
+
     return (
         <View style={styles.container}>
         <ImageBackground source={require('../assets/displayBackground.jpg')} style={styles.backgroundImage}>
-          <TextInput 
-            placeholder="Usernamee" 
+          
+        <Formik
+          initialValues={{ email: '', password: '' }}
+          onSubmit={(values, actions) => {
+            alert(JSON.stringify(values));
+            setTimeout(() => {
+              actions.setSubmitting(false);
+            }, 1000);
+          }}
+          validationSchema={validationSchema}>
+
+{formikProps => (
+        <React.Fragment>
+             <TextInput 
+            placeholder="Username" 
             style={styles.input}
             onChangeText = {(username) => this.setState({ username })}
+            onChangeText={formikProps.handleChange('email')}
+            onBlur={formikProps.handleBlur('email')}
+            autoFocus
             />
-        
-          <TextInput
+            <Text style={{ color: 'red' }}>
+              {formikProps.touched.email && formikProps.errors.email}
+            </Text>
+
+            <TextInput
             placeholder="Password"
             style={styles.input}
             onChangeText = {(password) => this.setState({ password })}
+            onChangeText={formikProps.handleChange('password')}
+            onBlur={formikProps.handleBlur('password')}
+            secureTextEntry
             />
-  
+            <Text style={{ color: 'red' }}>
+              {formikProps.touched.password && formikProps.errors.password}
+            </Text>
+
+          {formikProps.isSubmitting ? (
+            <ActivityIndicator />
+          ) : (
             <View style={styles.touchableOpacityView}>
-              <TouchableOpacity style={styles.touchableOpacitySign} onPress={()=>UserSignIn(this.state.username,this.state.password,null)}>
-                <Text style={styles.touchableOpacityTextSign}>Sign In</Text>
-              </TouchableOpacity>
-            </View>
-  
-            <View style={styles.touchableOpacityView}>
-              <TouchableOpacity style={styles.touchableOpacityRegister} onPress={()=>this.props.navigation.navigate('registerActivity')}>
+              <TouchableOpacity disabled={false} style={styles.touchableOpacityRegister} onPress={formikProps.handleSubmit}>
                 <Text style={styles.touchableOpacityTextRegister}>Register</Text>
               </TouchableOpacity>
             </View>
+          )}
+        </React.Fragment>
+      )}
+          </Formik>
         </ImageBackground>
       </View>
     );
