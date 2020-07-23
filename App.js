bb19be6750600cefb82a6f9f27e1df6256d97314
ahmadmarhaba <61730458+ahmadmarhaba@users.noreply.github.com>
2020-07-23 11:33:55 +0300

    asd

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import {createSwitchNavigator, createAppContainer} from "react-navigation";
 
 import signinActivity from "./screens/singinActivity";
 import registerActivity from "./screens/registerActivity";
-import registerActivity from "./Home/home";
+import home from "./Home/home";
 
 const App = createSwitchNavigator({
   signinActivity: {screen: signinActivity},
