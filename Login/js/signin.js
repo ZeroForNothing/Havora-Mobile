39c76a8431cc61d61879c41248e56fe687e5e0c1
ahmadmarhaba <dragon.8881@hotmail.com>
2020-07-26 22:05:58 +0300

    asd

--- Login/js/signin.js
+++ Login/js/signin.js
@@ -1,26 +1,28 @@
 export const UserSignIn = (actions, navigation, username, password, check) => {
-  if (username != null) {
-    navigation.navigate("home");
-  }else{
-    return false;
+  // if (username != null) {
+  //   navigation.navigate("home");
+  // }else{
+  //   return false;
+  // }
+  async function postData(url = '', data = {}) {
+    const response = await fetch(url, {
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      mode: 'cors',
+      method: 'post',
+      body: JSON.stringify(data)
+    });
+    return response.text();
   }
-// async function postData(url = '', data = {}) {
-//   const response = await fetch(url, {
-//     headers: {
-//       'Content-Type': 'application/json'
-//     },
-//     mode: 'cors',
-//     method: 'post',
-//     body: JSON.stringify(data)
-//   });
-//   return response.text();
-// }
-//
-// postData('https://89.40.13.203/SignIn', {username: 'whatever', password: 'whatever', check: null})
-//   .then(data => {
-//     alert(data);
-//
-//   }).catch(error => {
-//     console.error('There has been a problem with your fetch operation:', error);
-//   });
+  postData('https://89.40.13.203/SignIn', {
+      username: 'whatever',
+      password: 'whatever',
+      check: null
+    })
+    .then(data => {
+      alert(data);
+    }).catch(error => {
+      console.error('There has been a problem with your fetch operation:', error);
+    });
 };
